95874348b0ff6db41423df1063e99f394a2be64c
Vyacheslav Pryimak <incu6us@yandex.ru>
2016-11-25 11:40:55 +0200

    hotfix added without tests

--- README.md
+++ README.md
@@ -9,17 +9,15 @@ go run {main,graphql,models,handler}.go
 ## Examples to execute
 ### Time API
 ```
-curl -XPOST http://localhost:8081/graphql -H 'Content-Type: application/json' -d '{"query": "{timeQuery}"}'
+curl -XPOST http://localhost:8081/graphql -H 'Content-Type: application/json' -d '{"query": "{timeQuery {time,timestamp} }"}'
 ```
 
 ### Date API
 ```
-curl -XPOST http://localhost:8081/graphql -H 'Content-Type: application/json' -d '{"query": "{dateQuery}"}'
+curl -XPOST http://localhost:8081/graphql -H 'Content-Type: application/json' -d '{"query": "{dateQuery {date,timestamp} }"}'
 ```
 
 ### or with GET Method
 ```
-curl -XGET http://localhost:8081/graphql?query={timeQuery}
+curl -XGET http://localhost:8081/graphql?query={timeQuery {time,timestamp}}
 ```
-
-
--- src/graphql.go
+++ src/graphql.go
@@ -8,17 +8,41 @@ import (
 func GraphSchema() graphql.Schema {
   var Schema graphql.Schema
 
+  var timeType = graphql.NewObject(graphql.ObjectConfig{
+    Name: "time",
+    Fields: graphql.Fields{
+      "time": &graphql.Field{
+        Type: graphql.String,
+      },
+      "timestamp": &graphql.Field{
+        Type: graphql.String,
+      },
+    },
+  })
+
+  var dateType = graphql.NewObject(graphql.ObjectConfig{
+    Name: "date",
+    Fields: graphql.Fields{
+      "date": &graphql.Field{
+        Type: graphql.String,
+      },
+      "timestamp": &graphql.Field{
+        Type: graphql.String,
+      },
+    },
+  })
+
   var queryObject = graphql.NewObject(graphql.ObjectConfig{
     Name: "Query",
     Fields: graphql.Fields{
       "timeQuery": &graphql.Field{
-        Type: graphql.String,
+        Type: timeType,
         Resolve: func(p graphql.ResolveParams) (interface{}, error) {
           return GetTimeQuery(), nil
         },
       },
       "dateQuery": &graphql.Field{
-          Type: graphql.String,
+          Type: dateType,
           Resolve: func(p graphql.ResolveParams) (interface{}, error) {
             return GetDateQuery(), nil
           },
